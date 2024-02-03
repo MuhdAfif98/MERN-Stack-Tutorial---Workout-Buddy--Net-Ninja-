0cf4341fffa3a36e804c74466f7f6cdb98091d2c
Muhammad Afif <44563274+MuhdAfif98@users.noreply.github.com>
2024-02-03 23:52:41 +0800

    Init commit

---
+++ src/pages/Home.js
@@ -0,0 +1,37 @@
+import { useEffect } from "react";
+import { useWorkoutContext } from "../hooks/useWorkoutContext";
+
+// components
+import WorkoutDetails from "../components/WorkoutDetails";
+import WorkoutForm from "../components/WorkoutForm";
+
+const Home = () => {
+  const { workout, dispatch } = useWorkoutContext();
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch("/api/workouts");
+      const json = await response.json();
+
+      if (response.ok) {
+        dispatch({ type: "SET_WORKOUT", payload: json });
+      }
+    };
+
+    fetchWorkouts();
+  }, []);
+
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workout &&
+          workout.map((workout) => (
+            <WorkoutDetails key={workout._id} workout={workout} />
+          ))}
+      </div>
+      <WorkoutForm />
+    </div>
+  );
+};
+
+export default Home;
